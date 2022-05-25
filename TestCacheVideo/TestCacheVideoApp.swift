dea3de844a982c3ff861e64624baf2c473f700d9
Leonore Yardimli <leonore1982@gmail.com>
2022-05-25 15:27:01 +0800

    Initial Commit

---
+++ TestCacheVideo/TestCacheVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCacheVideoApp.swift
+//  TestCacheVideo
+//
+//  Created by Leonore Yardimli on 2022/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TestCacheVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
